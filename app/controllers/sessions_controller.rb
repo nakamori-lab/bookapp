07b8d86e0edeff64cf959614fd57ff1e4632eed7
中森海 <nakamorikai@nakamorikainoMacBook-Air.local>
2019-10-01 11:54:51 +0900

    アカウント有効化完成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,13 +5,20 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me] == '1' ? remember(user):forget(user)
-      redirect_back_or user
-    else
-      flash.now[:danger] = "invalid email/password combination"
-      render 'new'
-    end
+        if user.activated?
+          log_in user
+          params[:session][:remember_me] == '1' ? remember(user):forget(user)
+          redirect_back_or user
+        else
+          message = "アカウントが有効化されていません"
+          message += "あなたのメールの有効化リンクを確認してください"
+          flash[:warning] = message
+          redirect_to root_url
+        end
+      else
+        flash.now[:danger] = "invalid email/password combination"
+        render 'new'
+      end
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,11 @@ Rails.application.routes.draw do
   get '/about', to:'static_pages#about'
 
   get '/signup', to:'users#new'
-  resources :users
 
   get '/login', to:'sessions#new'
   post '/login', to:'sessions#create'
   delete '/logout', to:'sessions#destroy'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users
+  resources :account_activations,only:[:edit]
 end
